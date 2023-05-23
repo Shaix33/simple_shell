f0d2009c198712c2feabb34db1fb6df2f2881b16
Mumbi69 <winfriendmumbi69@gmail.com>
2023-05-23 12:52:13 +0000

    Simple Shell

--- shell.h
+++ shell.h
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 char *cmd = NULL, *cmd_cpy = NULL, *token = NULL, *delim = NULL, **argv = NULL;
 int argc = 0, x = 0;
 size_t buf = 0;
+
+void shell_interactive(void);
+
+void shell_no_interactive(void);
 
 #endif
