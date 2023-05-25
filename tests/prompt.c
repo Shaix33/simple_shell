cc811a5f70f54cbd1b9f1b97532e0834ef7d37ab
Shaix33 <KevinNdlondlo@gmail.com>
2023-05-24 18:41:36 -0700

    adding files

--- tests/prompt.c
+++ tests/prompt.c
@@ -1,44 +1,31 @@
 #include "shell.h"
+void sash(char **args);
+
 /**
- * Creating_prompt - displaying terminal prompt
- * 
- * Return: Always 0 Success
- */
-int main(void)
+* Creating_prompt - displaying terminal prompt
+*
+* Return: Always 0 Success
+*/
+
+int main(int argc, char **argv)
 {
+	(void) argc;
+	sash(argv);
+	return (0);
+}
+
+void sash(char **args)
+{
+	char *prompt = ("$ ");
+	char *input = NULL;
+	size_t len = 0;
+
 	while(1)
 	{
-		write(STDOUT_FILENO, prompt, 2);
-	
-		if (getline(&cmd, &buf, stdin) == -1)
-		{
-			return (-1);
-		}
-		cmd_cpy = strdup(cmd);
-		token = strtok(cmd, delim);
+		write(1, prompt, strlen(prompt));
+		if (getline(&input, &len, stdin) == -1)
+			exit(98);
 
-		while (token)
-		{
-			token = strtok(NULL, delim);
-			argc++;
-		}
-		argv = malloc(sizeof(char *) * argc);
-		token = strtok(cmd_cpy, delim);
-
-		while (token)
-		{
-			argv[x] = token;
-			token = strtok(NULL, delim);
-			x++;
-		}
-		argv[x] = NULL;
-		x = 0;
-
-		while(argv[x])
-		{
-			printf("%s\n", argv[x++]);
-		}
-		free(cmd), free(cmd_cpy), free(argv);
-		return (0);
 	}
+
 }
