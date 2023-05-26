044a0e022ba3d21f42c10eb80058e5e93a7ed3a8
Mumbi69 <winfriendmumbi69@gmail.com>
2023-05-26 11:33:01 +0000

    Simple Shell

--- loop.c
+++ loop.c
@@ -50,7 +50,7 @@ void shell_loop(data_shell *datash)
 	loop = 1;
 	while (loop == 1)
 	{
-		write(STDIN_FILENO, "^-^ ", 4);
+		write(STDIN_FILENO, "~$ ", 4);
 		input = read_line(&i_eof);
 		if (i_eof != -1)
 		{
